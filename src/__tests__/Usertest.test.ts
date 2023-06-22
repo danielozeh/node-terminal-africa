6f19fb86a54b531de9850c758009e3254d423729
Daniel Ozeh <danielozeh@gmail.com>
2023-06-22 11:45:35 +0100

    user module for terminal africa

---
+++ src/__tests__/Usertest.test.ts
@@ -0,0 +1,22 @@
+import TerminalAfrica from '../index';
+const userId = 'USER-12568655307'
+
+test('get user', async () => {
+    const user: any = await TerminalAfrica.getUser(userId)
+    expect(user.status).toBe(true)
+}, 10000);
+
+test('get user wallet', async () => {
+    const wallet: any = await TerminalAfrica.walletInfo(userId)
+    expect(wallet.status).toBe(true)
+}, 10000);
+
+test('get user wallet balance', async () => {
+    const wallet: any = await TerminalAfrica.walletBalance(userId)
+    expect(wallet.status).toBe(true)
+}, 10000);
+
+test('get user carriers', async () => {
+    const carriers: any = await TerminalAfrica.carriers()
+    expect(carriers.status).toBe(true)
+}, 10000);
---
+++ src/modules/user.ts
@@ -0,0 +1,62 @@
+import terminalAfricaInstance from "../services/terminal_africa"
+
+export default class User {
+    /**
+     * Get User - This allows you to fetch user information on Terminal Africa
+     * @param {string} userId
+     * @returns object
+     * @method GET
+     */
+    static async getUser(userId: string) {
+        try {
+            const resp = await terminalAfricaInstance.get(`/users/${userId}`)
+            return resp.data
+        } catch(err: any) {
+            return err.response.data
+        }
+    }
+
+    /**
+     * Get Wallet Info - This allows you to retrieve wallet information on Terminal Africa
+     * @param {string} userId
+     * @returns object
+     * @method GET
+     */
+    static async walletInfo(userId: string) {
+        try {
+            const resp = await terminalAfricaInstance.get(`/users/wallet?user_id=${userId}`)
+            return resp.data
+        } catch(err: any) {
+            return err.response.data
+        }
+    }
+
+    /**
+     * Get Wallet Balance - This allows you to retrieve wallet balance on Terminal Africa
+     * @param {string} userId
+     * @returns object
+     * @method GET
+     */
+    static async walletBalance(userId: string) {
+        try {
+            const resp = await terminalAfricaInstance.get(`/users/wallet-balance?user_id=${userId}`)
+            return resp.data
+        } catch(err: any) {
+            return err.response.data
+        }
+    }
+
+    /**
+     * Get User Carriers - This allows you to fetch acivated carriers on Terminal Africa
+     * @returns object
+     * @method GET
+     */
+    static async carriers() {
+        try {
+            const resp = await terminalAfricaInstance.get(`/users/carriers`)
+            return resp.data
+        } catch(err: any) {
+            return err.response.data
+        }
+    }
+}
